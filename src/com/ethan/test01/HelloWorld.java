2139b027345f7b96940687d34928343af2861d0d
Ethan <Ethan@qq.com>
2024-10-13 08:57:59 +0800

    second commit

--- src/com/ethan/test01/HelloWorld.java
+++ src/com/ethan/test01/HelloWorld.java
@@ -3,6 +3,7 @@ package com.ethan.test01;
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello World!!");
+        System.out.println("Hello World!!");
     }
 }
 
